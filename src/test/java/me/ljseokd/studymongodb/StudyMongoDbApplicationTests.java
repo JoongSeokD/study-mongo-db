b2a4199af31d784c72dd5d1747019090fe2ef232
JoongSeokD <ljseokd@gmail.com>
2020-07-03 01:32:45 +0900

    예제

---
+++ src/test/java/me/ljseokd/studymongodb/StudyMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ljseokd.studymongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
